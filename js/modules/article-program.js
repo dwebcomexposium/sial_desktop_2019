a3ab7c0f0a8520f5e9ff9d5e504b4cef404660d6
Jerem <jeremie.nehlil@fidesio.com>
2019-11-19 15:45:16 +0100

    correction css sur page sous detail evenement

--- js/modules/article-program.js
+++ js/modules/article-program.js
@@ -7,13 +7,13 @@
       var socialSharingDiv = $('.social-sharing')[0];
       var divHeight = socialSharingDiv.getBoundingClientRect().height;
       var titleProps = $('.article-program .article-title')[0].getBoundingClientRect();
-      var topPos = parseInt(titleProps.top, 10) + (parseInt(titleProps.height) / 5) - parseInt(divHeight, 10);
+      var topPos = parseInt(titleProps.top, 10) + (parseInt(titleProps.height) / 4) - parseInt(divHeight, 10);
       socialSharingDiv.style.top = topPos + 'px';
     };
 
     if ($('body').hasClass('article-program')) {
       calculateSocialSharingPos();
-      window.resize(function() {
+      $(window).resize(function() {
         calculateSocialSharingPos();
       });
     }
