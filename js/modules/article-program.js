f44275eead897ba42b38ba37ccaae00961b75ca9
Jerem <jeremie.nehlil@fidesio.com>
2019-11-21 17:06:08 +0100

    update

--- js/modules/article-program.js
+++ js/modules/article-program.js
@@ -4,12 +4,15 @@
 (function ($) {
   $(function () {
     var calculateSocialSharingPos = function() {
-      var socialSharingDiv = $('.social-sharing')[0];
       $('.social-sharing .sr-title').text('Partager cet évènement');
-      var divHeight = socialSharingDiv.getBoundingClientRect().height;
-      var titleProps = $('.article-program .article-title img').offset().top;
-      var topPos = parseInt(titleProps, 10) - parseInt(divHeight, 10) - 40;
-      socialSharingDiv.style.top = topPos + 'px';
+      if (isDesktopMedium) {
+        var isDesktopMedium = window.matchMedia("(min-width: 1280px)")
+        var socialSharingDiv = $('.social-sharing')[0];
+        var divHeight = socialSharingDiv.getBoundingClientRect().height;
+        var titleProps = $('.article-program .article-title img').offset().top;
+        var topPos = parseInt(titleProps, 10) - parseInt(divHeight, 10) - 40;
+        socialSharingDiv.style.top = topPos + 'px';
+      }
     };
 
     if ($('body').hasClass('article-program') || $('body').hasClass('article-node1310667')) {
