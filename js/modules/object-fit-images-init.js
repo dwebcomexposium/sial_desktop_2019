8e9b5f677e9492aefb409dc747763cfb8b6deeb1
jeremN <jeremne@gmail.com>
2019-12-06 10:34:49 +0100

    update

--- js/modules/object-fit-images-init.js
+++ js/modules/object-fit-images-init.js
@@ -1,9 +1,1 @@
-/**
- * @requires objectFitImages
- */
-(function($) {
-    $(function() {
-        objectFitImages();
-    });
-})(jQuery);
-//# sourceMappingURL=object-fit-images-init.js.map
+jQuery(function(){objectFitImages()});
