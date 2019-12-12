33d64af937ca982f5b7db7754615090fea81da19
Guillaume Bouillon <guillaume.bouillon@fidesio.com>
2019-12-12 11:21:12 +0100

    FIX: remove dev files

--- js/modules/block-page-faq.js
+++ js/modules/block-page-faq.js
@@ -1,11 +1,1 @@
-(function($) {
-    $(function() {
-        /* Removes empty html tags from Wysiwyg to prevent margin issues */
-        $('body.faq .block-page.faq .fc-item-content > *').each(function() {
-            if ($(this).text().trim().length === 0) {
-                $(this).remove();
-            }
-        });
-    });
-})(jQuery);
-//# sourceMappingURL=block-page-faq.js.map
+!function(t){t(function(){t("body.faq .block-page.faq .fc-item-content > *").each(function(){0===t(this).text().trim().length&&t(this).remove()})})}(jQuery);
