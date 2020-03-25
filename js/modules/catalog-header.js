665c690f7a31f4f6316660b9b06fe12f96e44967
Sade <Sade@users.noreply.github.com>
2020-03-25 17:45:36 +0100

    pv#115647 - Move intro text to exhibitors catalogue header

---
+++ js/modules/catalog-header.js
@@ -0,0 +1,1 @@
+!function(o){o(function(){var e=o(".catalogue.exposant .esf-search-line"),a=o(".catalogue.exposant #zone1 > p:first-child").addClass("js-moved esf-intro-text");e.before(a)})}(jQuery);
