f9e4d0f527b0bc0da6f07eec9bc408ff0b3724d5
Etienne <etienne.moureton@unflux.fr>
2022-05-24 12:53:30 +0200

    Update scroll.js

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -42,7 +42,6 @@ const backToTop = () => {
   el.outerHTML = '<div class="back-to-top-button"><svg viewBox="0 0 256 256" version="1.1" style="fill: rgba(166, 166, 166, 0.9);"><path d="M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z"></path></svg></div>'
   
   const element = document.querySelector('.back-to-top-button')
-  console.log(element)
 
   element.addEventListener('click', () => {
     document.body.scrollTop = 0;
@@ -52,10 +51,8 @@ const backToTop = () => {
   document.addEventListener('scroll', () => {
     console.log(window.scrollY)
     if(window.scrollY > 400) {
-      console.log('visible')
       element.classList.add('visible')
     } else {
-      console.log('non visible')
       element.classList.remove('visible')
     }
   })
