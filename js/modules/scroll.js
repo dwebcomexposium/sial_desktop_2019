b189f31d0c4f68bc93704929a9131f2666cda669
Etienne <etienne.moureton@unflux.fr>
2022-05-24 12:57:01 +0200

    Update scroll.js

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -39,14 +39,9 @@ const backToTop = () => {
   const body = document.body
   const el = document.createElement('div')
   body.appendChild(el)
-  el.outerHTML = '<div class="back-to-top-button"><svg viewBox="0 0 256 256" version="1.1" style="fill: rgba(166, 166, 166, 0.9);"><path d="M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z"></path></svg></div>'
+  el.outerHTML = '<a href="#top" class="back-to-top-button"><svg viewBox="0 0 256 256" version="1.1" style="fill: rgba(166, 166, 166, 0.9);"><path d="M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z"></path></svg></a>'
   
   const element = document.querySelector('.back-to-top-button')
-
-  element.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  })
 
   document.addEventListener('scroll', () => {
     console.log(window.scrollY)
