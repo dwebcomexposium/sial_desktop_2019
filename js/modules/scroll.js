346baa2a15e975f8607511a2883dbb23a08d07d5
Etienne <etienne.moureton@unflux.fr>
2022-05-24 12:49:38 +0200

    Update scroll.js

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -41,7 +41,7 @@ const backToTop = () => {
   body.appendChild(el)
   el.outerHTML = '<div class="back-to-top-button"><svg viewBox="0 0 256 256" version="1.1" style="fill: rgba(166, 166, 166, 0.9);"><path d="M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z"></path></svg></div>'
   
-  const element = document.querySelector('back-to-top-button')
+  const element = document.querySelector('.back-to-top-button')
   console.log(element)
 
   element.addEventListener('click', () => {
