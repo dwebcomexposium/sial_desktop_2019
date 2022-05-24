152f4aa168c3fabe8ac333ab703ef5e31a4fef8f
Etienne <etienne.moureton@unflux.fr>
2022-05-24 12:20:30 +0200

    Update scroll.js

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -40,7 +40,7 @@ console.log('scroll loaded')
 const backToTop = () => {
   const body = document.body
   const el = document.createElement('div')
-  el.className.add('back-to-top')
+  // el.className.add('back-to-top')
   const svg = document.createElement('svg')
   svg.outerHTML = '<svg class="back-to-top" viewBox="0 0 256 256" version="1.1" style="fill: rgba(166, 166, 166, 0.9);"><path d="M88.4020203,153.455844 L128,113.857864 L167.59798,153.455844 L173.254834,147.79899 L128,102.544156 L125.171573,105.372583 L82.745166,147.79899 L88.4020203,153.455844 Z"></path></svg>'
   el.appendChild(svg)
